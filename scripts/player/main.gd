3bb0a1ad62a1c53c6e9e89233b16e0e28584b64d
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2025-10-06 19:09:32 -0700

    Initial commit

---
+++ scripts/player/main.gd
@@ -0,0 +1,139 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+@onready var anim = $invincible
+@onready var coyote_collider = $coyote
+@onready var floor_ray = $floor_ray
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
+var spawn : Vector2
+var coyote = false
+var root
+var UI
+var health = 5
+var last_health = 5
+var main
+var last_direction = 0
+var invincible = false
+var can_move = true
+var took_dmg = false
+var is_jumping = false
+@export var can_dash = true
+var is_dashing = false
+var unlocked_dash = true
+
+func _ready() -> void:
+	spawn = self.global_position
+	root = get_tree().root
+	main = root.get_node("main")
+	UI = main.get_node("Sub").get_node("UI")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if unlocked_dash and is_on_floor():
+		can_dash = true
+	if took_dmg == true:
+		self.global_position = self.global_position
+	if invincible == false:
+		$invincible.stop()
+	if health != last_health:
+		UI.health = health
+		UI.update_health()
+		last_health = health
+		
+	if not is_on_floor() and is_jumping == false:
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if (floor_ray.is_colliding() == false or Input.is_action_pressed("ui_accept") == false):
+		is_jumping = false
+	if Input.is_action_pressed("ui_accept") and (is_on_floor() or coyote) and can_move and floor_ray.is_colliding():
+		velocity.y = JUMP_VELOCITY
+		is_jumping = true
+	if Input.is_action_pressed("ui_accept") and can_move and floor_ray.is_colliding() and is_jumping:
+		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if (velocity.x == 0):
+		is_dashing = false
+	if direction == 0:
+		match last_direction:
+			0.0:
+				sprite.play("idle_down")
+			1.0:
+				sprite.play("idle_side")
+				sprite.flip_h = false
+			-1.0:
+				sprite.play("idle_side")
+				sprite.flip_h = true
+	else:
+		match direction:
+			1.0:
+				sprite.play("walk_side")
+				sprite.flip_h = false
+			-1.0:
+				sprite.play("walk_side")
+				sprite.flip_h = true
+		last_direction = direction
+	
+	if direction and can_move and !Input.is_action_just_pressed("dash") and is_dashing == false:
+		velocity.x = direction * SPEED
+	else:
+		if (!Input.is_action_just_pressed("dash") and is_dashing == false):
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	if can_dash and Input.is_action_just_pressed("dash"):
+		velocity.x = last_direction * (SPEED * 3)
+		is_dashing = true
+		stop_dash()
+	
+	move_and_slide()
+
+
+func stop_dash():
+	await get_tree().create_timer(0.2).timeout
+	velocity.x = 0
+	can_dash = false
+	move_and_slide()
+
+func _on_spike_check_body_entered(body: Node2D) -> void:
+	took_dmg = true
+	self.global_position = self.global_position
+	$CPUParticles2D.emitting = true
+	jerk_camera()
+	await $jerk.animation_finished
+	UI.fade_in()
+	await get_tree().create_timer(0.5).timeout
+	took_dmg  = false
+	anim.stop()
+	
+	self.global_position = spawn
+	deduct_health(1)
+
+
+func jerk_camera():
+	$jerk.play("jerk")
+	
+	
+func deduct_health(value : int):
+	if (invincible == false):
+		invincible = true
+		can_move = false
+		health = health - value
+		
+		invincible = true
+		anim.play("invincibility_frames")
+		UI.fade_out()
+		await get_tree().create_timer(1.5).timeout
+		invincible = false
+		can_move = true
+
+
+func _on_coyote_area_entered(area: Area2D) -> void:
+	coyote = true
+
+
+func _on_coyote_area_exited(area: Area2D) -> void:
+	coyote = false
---
+++ scripts/player/ui.gd
@@ -0,0 +1,66 @@
+extends SubViewport
+var health = 5
+var max_health = 0
+@onready var fade = $Control/fade
+@onready var health_nodes = {
+	1:$Control/Panel/HBoxContainer/PanelContainer/HBoxContainer/hp1,
+	2:$Control/Panel/HBoxContainer/PanelContainer/HBoxContainer/hp2,
+	3:$Control/Panel/HBoxContainer/PanelContainer/HBoxContainer/hp3,
+	4:$Control/Panel/HBoxContainer/PanelContainer/HBoxContainer/hp4,
+	5:$Control/Panel/HBoxContainer/PanelContainer/HBoxContainer/hp5
+	}
+	
+var full_heart_texture = preload("res://assets/images/sfb.png")
+var empty_heart_texture = preload("res://assets/images/empty_sfb.png")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func update_health():
+	match health:
+		0:
+			health_nodes.get(1).texture = empty_heart_texture
+			health_nodes.get(2).texture = empty_heart_texture
+			health_nodes.get(3).texture = empty_heart_texture
+			health_nodes.get(4).texture = empty_heart_texture
+			health_nodes.get(5).texture = empty_heart_texture
+		1:
+			health_nodes.get(1).texture = full_heart_texture
+			health_nodes.get(2).texture = empty_heart_texture
+			health_nodes.get(3).texture = empty_heart_texture
+			health_nodes.get(4).texture = empty_heart_texture
+			health_nodes.get(5).texture = empty_heart_texture
+		2:
+			for iteration in 1:
+				health_nodes.get(iteration+1).texture = full_heart_texture
+			health_nodes.get(3).texture = empty_heart_texture
+			health_nodes.get(4).texture = empty_heart_texture
+			health_nodes.get(5).texture = empty_heart_texture
+		3:
+			for iteration in 2:
+				health_nodes.get(iteration+1).texture = full_heart_texture
+			health_nodes.get(4).texture = empty_heart_texture
+			health_nodes.get(5).texture = empty_heart_texture
+		4:
+			for iteration in 3:
+				health_nodes.get(iteration+1).texture = full_heart_texture
+			health_nodes.get(5).texture = empty_heart_texture
+		5:
+			for iteration in 4:
+				health_nodes.get(iteration+1).texture = full_heart_texture
+		_:
+			health = 0
+			update_health()
+			
+
+
+func fade_in():
+	fade.play("fade")
+	
+func fade_out():
+	fade.play_backwards("fade")
